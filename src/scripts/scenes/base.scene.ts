90761d8393269a35cb2ddae7c3b93dc200d9a78e
Yann Braga <yannbf@gmail.com>
2020-03-06 16:32:19 +0100

    feat(base-scene): add helper camera properties

--- src/scripts/scenes/base.scene.ts
+++ src/scripts/scenes/base.scene.ts
@@ -6,4 +6,24 @@ export class BaseScene extends Phaser.Scene {
   get isMobile() {
     return this.game.device.os.iOS || this.game.device.os.android
   }
+
+  get mainCamera() {
+    return this.cameras.main
+  }
+
+  get halfWidth() {
+    return this.mainCamera.width / 2
+  }
+
+  get halfHeight() {
+    return this.mainCamera.height / 2
+  }
+
+  get width() {
+    return this.mainCamera.width
+  }
+
+  get height() {
+    return this.mainCamera.height
+  }
 }
